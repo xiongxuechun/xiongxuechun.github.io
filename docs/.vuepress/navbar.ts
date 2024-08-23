ceb7349489ad08b0271bb12694b96e86fbec87b9
xiongxc <xiongxc@rd.netease.com>
2024-08-23 09:41:46 +0800

    navbar

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,57 +1,60 @@
-import { navbar } from "vuepress-theme-hope";
+import {navbar} from "vuepress-theme-hope";
 
 // 图标：https://theme-hope.vuejs.press/zh/guide/interface/icon.html#%E8%AE%BE%E7%BD%AE%E5%9B%BE%E6%A0%87
 // https://fontawesome.com/search?m=free&o=r
 // 专题话题的路径需在尾部添加 /，否则有可能出现链接错误。比如下方「生活」中的 baby/
 export default navbar([
-  { text: "个人博客", icon: "fa6-solid:blog", link: "/blog" },
-  {
-    text: "Java进阶",
-    icon: "fa6-solid:bars-staggered",
-    prefix: "/",
-    children: [
-      "apps/Applist",
-      {
-        text: "Java 并发",
-        icon: "fa6-brands:chrome",
-        link: "apps/Chrome",
-      },
-      {
-        text: "服务/专题",
-        icon: "",
-        prefix: "",
+    {text: "个人博客", icon: "fa6-solid:blog", link: "/blog"},
+    {text: "个人博客", icon: "fa6-solid:blog", link: "/blog"},
+    {text: "个人博客", icon: "fa6-solid:blog", link: "/blog"},
+
+    {
+        text: "Java进阶",
+        icon: "fa6-solid:fa-brands fa-java",
+        prefix: "/",
         children: [
-          {
-            text: "专题示例",
-            icon: "fa6-solid:dice-d20",
-            link: "apps/topic/",
-          },
+            "apps/Applist",
+            {
+                text: "Java 并发",
+                icon: "fa6-brands:chrome",
+                link: "apps/Chrome",
+            },
+            {
+                text: "服务/专题",
+                icon: "",
+                prefix: "",
+                children: [
+                    {
+                        text: "专题示例",
+                        icon: "fa6-solid:dice-d20",
+                        link: "apps/topic/",
+                    },
+                ],
+            },
         ],
-      },
-    ],
-  },
-  {
-    text: "算法",
-    icon: "fa6-solid:bed-pulse",
-    prefix: "/family/",
-    children: ["Diet", "Coupon"],
-  },
-  {
-    text: "工具",
-    icon: "fa6-solid:toolbox",
-    children: [
-      {
-        text: "ChatGPT SC",
-        icon: "fa6-solid:bolt",
-        link: "https://www.aishort.top/",
-      },
-      { text: "IMGPrompt", icon: "fa6-solid:image", link: "https://prompt.newzone.top/" },
-      { text: "多语言翻译", icon: "fa6-solid:language", link: "https://tools.newzone.top/json-translate" },
-      {
-        text: "工具收藏",
-        icon: "fa6-solid:bars",
-        link: "https://nav.newzone.top/",
-      },
-    ],
-  },
+    },
+    {
+        text: "算法",
+        icon: "fa6-solid:bed-pulse",
+        prefix: "/family/",
+        children: ["Diet", "Coupon"],
+    },
+    {
+        text: "工具",
+        icon: "fa6-solid:toolbox",
+        children: [
+            {
+                text: "ChatGPT SC",
+                icon: "fa6-solid:bolt",
+                link: "https://www.aishort.top/",
+            },
+            {text: "IMGPrompt", icon: "fa6-solid:image", link: "https://prompt.newzone.top/"},
+            {text: "多语言翻译", icon: "fa6-solid:language", link: "https://tools.newzone.top/json-translate"},
+            {
+                text: "工具收藏",
+                icon: "fa6-solid:bars",
+                link: "https://nav.newzone.top/",
+            },
+        ],
+    },
 ]);
